ee3582b93406080f66bc58aa3a488e747c4543c0
Tiago Palte <tpalte@redventures.com>
2022-02-25 10:35:39 -0500

    implements field number

--- controllers/controller.go
+++ controllers/controller.go
@@ -35,14 +35,34 @@ func (c Controller) Index(w http.ResponseWriter, r *http.Request) {
 
 func (c Controller) GetBillSale(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Query().Get("url")
-	fmt.Println(url)
-	billSale, err := c.service.GetBillSaleByUrl(url)
-	if err != nil {
-		fmt.Println(err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+	number := r.URL.Query().Get("number")
+	uf := r.URL.Query().Get("uf")
+
+	if url != "" {
+		fmt.Println("URL", url)
+		billSale, err := c.service.GetBillSaleByUrl(url)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(billSale)
+	} else if number != "" && uf != "" {
+		fmt.Println("Number", number)
+		fmt.Println("UF", uf)
+		billSale, err := c.service.GetBillSaleByNumber(number, uf)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(billSale)
+	} else {
+		http.Error(w, "Inform URL or Number", http.StatusInternalServerError)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(billSale)
 }
